65e0c9886065db3c8c8feffd9f9d2e3c585243a4
oorbea <meck.porky@gmail.com>
2024-08-09 01:06:33 +0200

    Player class created

---
+++ player.cc
@@ -0,0 +1,55 @@
+#include "player.hh"
+using namespace std;
+
+typedef vector<pair<string, string>> hakiList;
+
+//Constructor
+Player::Player(string& name) {
+    this->name = name;
+}
+
+//Modifiers
+void Player::setFruit(Fruit& fruit) {
+    this->devilFruit = fruit;
+}
+
+void Player::setStand(Stand& stand) {
+    this->standPossessed = stand;
+}
+
+void Player::setStrength(string& strength) {
+    this->baseStrength = strength;
+}
+
+void Player::setHaki(hakiList& haki) {
+    this->haki = haki;
+}
+
+void Player::setSpin(string& spin) {
+    this->spinMastery = spin;
+}
+
+//Consultors
+Fruit Player::getFruit() {
+    return this->devilFruit;
+}
+
+string Player::getFruitMastery() {
+    return this->fruitMastery;
+}
+
+Stand Player::getStand() {
+    return this->standPossessed;
+}
+
+string Player::getStrength() {
+    return this->baseStrength;
+}
+
+hakiList Player::getHaki() {
+    return this->haki;
+}
+
+string Player::getSpin() {
+    return this->spinMastery;
+}
--- player.hh
+++ player.hh
@@ -1,12 +1,49 @@
 #ifndef PLAYER_HH_
 #define PLAYER_HH_
 
+#include "fruit.hh"
+#include "stand.hh"
+#include <vector>
+
+typedef vector<pair<string, string>> hakiList;
 class Player
 {
+public:
+    //Constructor
+    Player(string& name);
+
+    //Modifiers
+    void setFruit(Fruit& fruit);
+
+    void setStand(Stand& stand);
+
+    void setStrength(string& strength);
+
+    void setHaki(hakiList& haki);
+
+    void setSpin(string& spin);
+
+    //Consultors
+    Fruit getFruit();
+
+    string getFruitMastery();
+
+    Stand getStand();
+
+    string getStrength();
+
+    hakiList getHaki();
+
+    string getSpin();
+
 private:
     string name;
-public:
-    
+    Fruit devilFruit;
+    string fruitMastery;
+    Stand standPossessed;
+    string baseStrength;
+    hakiList haki;
+    string spinMastery;
 };
 
 #endif
