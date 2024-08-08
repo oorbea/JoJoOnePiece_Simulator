bb33b0ff4447fca60148ca617a46e4e6396bf52b
oorbea <meck.porky@gmail.com>
2024-08-09 00:22:51 +0200

    Update fruit.cc

--- fruit.cc
+++ fruit.cc
@@ -7,18 +7,32 @@ Fruit::Fruit() {
 }
 
 //Modifiers
-    void Fruit::setName(string& name) {
-        this->name = name;
-    }
+void Fruit::setName(string& name) {
+    this->name = name;
+}
 
-    void Fruit::setDescription(string& description) {
-        this->description = description;
-    }
+void Fruit::setDescription(string& description) {
+    this->description = description;
+}
 
-    void Fruit::setType(string& type) {
-        this->type = type;
-    }
+void Fruit::setType(string& type) {
+    this->type = type;
+}
 
-    bool Fruit::exists() {
-        return name.length() > 0;
-    }
+//Consultors
+
+string Fruit::getName() {
+    return this->name;
+}
+
+string Fruit::getDescription() {
+    return this->description;
+}
+
+string Fruit::getType() {
+    return this->type;
+}
+
+bool Fruit::exists() {
+    return name.length() > 0;
+}
