b5872a924774d7f4124b65287a119258aedf958c
oorbea <meck.porky@gmail.com>
2024-08-09 16:52:18 +0200

    Language changed

--- main.cc
+++ main.cc
@@ -8,16 +8,17 @@ int main() {
     int numPlayers;
     cout<<"Welcome to JoJo's & OnePiece Simulator"<<endl;
     cout<<"En este simulador se os asignaran diversas cualidades y habilidades de los respectivos universos de One Piece y JoJo's Bizarre Adventure, con el objetivo de que discutais y decidais, basado en las condiciones de vuestros personajes, que tan lejos llegariais en el mundo de One Piece."<<endl;
-    cout<<"Introdueix el nombre de jugadors: ";
+    cout<<"Introduce el numero de jugadores: ";
     cin>>numPlayers;
     cout<<endl;
     vector<Player> playersInRoom(numPlayers);
-    cout<<"Introduiex el nom dels jugadors: "<<endl;
+    cout<<"Introduce el numero de jugadores: "<<endl;
     for(int i=0; i<numPlayers; i++){
-        cout<<"Player "<<i + 1<<": ";
+        cout<<"Jugador "<<i + 1<<": ";
         string name;
         cin>>name;
         playersInRoom[i].setName(name);
     }
+    cout << "Muy bien, asignemos poderes a los jugadores" << endl;
 
 }
