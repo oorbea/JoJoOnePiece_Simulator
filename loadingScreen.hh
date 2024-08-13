a5c1048e8f84777f259e878b816582678c4e904d
oorbea <meck.porky@gmail.com>
2024-08-13 17:26:29 +0200

    Squashed commit of the following:
    commit 1ec3261494a1714777009fb49678ed365442383d
    Author: oorbea <meck.porky@gmail.com>
    Date:   Tue Aug 13 17:26:09 2024 +0200
    
        Build file created

--- loadingScreen.hh
+++ loadingScreen.hh
@@ -1,3 +1,6 @@
+#ifndef LOADING_HH_
+#define LOADING_HH_
+
 #include <iostream>
 #include <thread>
 #include <chrono>
@@ -7,3 +10,5 @@ using namespace std;
 void delay(double secs);
 
 void loadingScreen(int times);
+
+#endif
