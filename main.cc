2a91c873a5e247804f9f4ceb7df2ada22d6f5976
oorbea <meck.porky@gmail.com>
2024-08-13 17:35:36 +0200

    Ticks added

--- main.cc
+++ main.cc
@@ -7,12 +7,12 @@ int main() {
 
     int numPlayers;
     cout << "Welcome to JoJo's & OnePiece Simulator" << endl;
-    cout << "En este simulador se os asignarán diversas cualidades y habilidades de los respectivos universos de One Piece y JoJo's Bizarre Adventure, con el objetivo de que discutáis y decidáis, basado en las condiciones de vuestros personajes, que tan lejos llegaríais en el mundo de One Piece." << endl;
-    cout << "Introduce el numero de jugadores: ";
+    cout << "En este simulador se os asignarán diversas cualidades y habilidades de los respectivos universos de One Piece y JoJo's Bizarre Adventure, con el objetivo de que discutáis y decidáis, basado en las condiciones de vuestros personajes, cuán lejos llegaríais en el mundo de One Piece." << endl;
+    cout << "Introduce el número de jugadores: ";
     cin >> numPlayers;
     cout << endl;
     vector<Player> playersInRoom(numPlayers);
-    cout << "Introduce el nombre de los jugadores: " << endl;
+    cout << "Introduce el nombre de cada jugador: " << endl;
     for(int i=0; i<numPlayers; i++) {
         cout << "Jugador " << i + 1 << ": ";
         string name;
@@ -25,7 +25,7 @@ int main() {
         string playerName = player->getName();
         cout << "Le toca a " << playerName << endl << endl;
 
-        cout << "Primero veamos tu fisico base" << endl;
+        cout << "Primero veamos tu físico base" << endl;
         loadingScreen(generateRandomNumber(2, 5));
         string strength = generateStrength();
         player->setStrength(strength);
@@ -42,7 +42,7 @@ int main() {
             cout << playerName << ", tu nivel de spin es " << spin << endl << endl;
         delay(2.5);
 
-        cout << "Vamos, a ver que tal tu fruta del diablo" << endl;
+        cout << "Vamos, a ver qué tal tu fruta del diablo" << endl;
         loadingScreen(generateRandomNumber(2, 5));
         Fruit akuma = generateFruit();
         player->setFruit(akuma);
@@ -50,17 +50,17 @@ int main() {
             cout << playerName << ", tu fruta es la " << akuma.getName() << ", que es de tipo " << akuma.getType() << endl << akuma.getDescription() << endl << endl;
             delay(5);
 
-            cout << "Y tu maestria con ella es..." << endl;
+            cout << "Y tu maestría con ella es..." << endl;
             loadingScreen(generateRandomNumber(2, 5));
             string fruitMas = generateFruitMastery();
             player->setFruitMastery(fruitMas);
-            cout << "Maestria con tu fruta: " << fruitMas << endl << endl;
+            cout << "Maestría con tu fruta: " << fruitMas << endl << endl;
         }
         else
             cout << playerName << ", lo siento, pero desafortunadamente no tienes fruta del diablo." << endl << endl;
         delay(2.5);
 
-        cout << "¡Algo te esta pasando! Creo que vas a obtener un stand" << endl;
+        cout << "¡Algo te está pasando! ¡Creo que vas a obtener un stand!" << endl;
         loadingScreen(generateRandomNumber(2, 5));
         Stand stand = generateStand();
         player->setStand(stand);
@@ -74,7 +74,7 @@ int main() {
             delay(2.5);
         }
 
-        cout << "A ver que tal los resultados de tu entrenamiento. ¿Que haki tendras?" << endl;
+        cout << "A ver qué tal los resultados de tu entrenamiento. ¿Qué haki tendrás?" << endl;
         loadingScreen(generateRandomNumber(2, 5));
         hakiList haList = generateHaki();
         if (haList[0].first != "Sin haki") {
@@ -86,7 +86,7 @@ int main() {
             delay(2.5);
             cout << endl << endl;
             
-            cout << "Y tu maestria con cada uno de ellos es: " << endl;
+            cout << "Y tu maestría con cada uno de ellos es: " << endl;
             loadingScreen(generateRandomNumber(2, 5));
             haList = generateHakiMastery(haList);
             for (int j = 0; j < haList.size(); ++j) {
